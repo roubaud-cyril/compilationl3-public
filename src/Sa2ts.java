00e03d4b3afe7154331f4a518b288e46da8dfdc9
Adrien Tokoglu <adrien.tokoglu@etu.univ-amu.fr>
2020-02-21 16:18:24 +0100

    debut tp3

---
+++ src/Sa2ts.java
@@ -0,0 +1,33 @@
+import sa.*;
+
+public class Sa2ts extends SaDepthFirstVisitor <Void> {
+    @Override
+    public Void visit(SaDecTab node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public Void visit(SaDecFonc node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public Void visit(SaDecVar node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public Void visit(SaVarSimple node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public Void visit(SaVarIndicee node) {
+        return super.visit(node);
+    }
+
+    @Override
+    public Void visit(SaAppel node) {
+        return super.visit(node);
+    }
+}
